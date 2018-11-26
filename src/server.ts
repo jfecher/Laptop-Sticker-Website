5b8d7becf2ab059f464d7475d513e3d45e2c11e6
David Padawer <d.padawer@gmail.com>
2018-11-25 19:34:51 -0500

    initial setup (hello world)

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+
+var port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log('Example app listening on port ' + port));
