cccbfb97d236c9b6e1376a706912d1618607fee4
David Padawer <d.padawer@gmail.com>
2018-11-26 13:41:03 -0500

    first query

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,30 @@ client.connect();
 var port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
-app.get('/api/', (req, res) => res.json({'xaxis':req.param('xaxis'), 'yaxis':req.param('yaxis')}));
+app.get('/api/',
+    (req, res) =>
+    {
+        const needsMajorJoin = req.param('xaxis') == 'major' || req.param('xaxis') == 'major';
+        let queryString = "select * from person";
+        if (needsMajorJoin)
+        {
+            queryString += " join major using (major_id)"
+        }
+        queryString += " limit 5;";
+        const query = client.query(queryString);
+
+        query.then(
+            (result : any) =>
+            {
+                res.json(result.rows);
+            }
+        ).catch(
+            (err : any) =>
+            {
+                console.log(err)
+            }
+        );
+    }
+);
 
 app.listen(port, () => console.log('Example app listening on port ' + port));
