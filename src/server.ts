4c86e54f823d3853f776e48d9ddeb3fe011eb810
David Padawer <d.padawer@gmail.com>
2018-11-26 13:17:52 -0500

    db api setup

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,6 @@ client.connect();
 var port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
-app.get('/api/', (req, res) => res.json({'req':req}));
+app.get('/api/', (req, res) => res.json({'xaxis':req.param('xaxis'), 'yaxis':req.param('yaxis')}));
 
 app.listen(port, () => console.log('Example app listening on port ' + port));
