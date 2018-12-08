496ff4acece0eaddb9b7b88bc5fc1533b903e7db
Jake Fecher <jfecher11@gmail.com>
2018-12-08 12:52:29 -0500

    Add /preview

--- src/gallery.ts
+++ src/gallery.ts
@@ -2,6 +2,6 @@ function fillGallery(sqlResults) {
     var container = document.getElementById("myNumPersons");
     container.innerHTML = "";
     sqlResults.laptop_picture_urls.for_each( url => {
-        container.innerHTML += "<img src='" + url + "' />";
+    container.innerHTML += "<img src='" + url + "/preview' />";
     });
 }
