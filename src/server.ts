17e3c8519986c611fd8424b0044cf2c7a985aaef
David Padawer <d.padawer@gmail.com>
2018-12-08 13:44:28 -0500

    experiment for cross origin stuff

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,33 @@ const connectionString = process.env.DATABASE_URL || '';
 const client = new pg.Client(connectionString);
 client.connect();
 
+
+//set up for cross origin
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://drive.google.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
+
+
+
+
 var port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
