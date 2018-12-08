23922e43e91abf1b2d3a55bea2c1e5d696eb5359
jmanlikescake <jalex609@gmail.com>
2018-12-07 21:01:44 -0500

    Fixed this shit

--- src/homepage.ts
+++ src/homepage.ts
@@ -2,7 +2,7 @@ var numPeople;
 function addHome(sqlResults) {
     console.log(sqlResults)
     var headingElement = <HTMLHeadingElement> document.getElementById("myNumPersons");
-    headingElement.innerHTML = sqlResults.numPeople;
+    headingElement.innerHTML = sqlResults.numPeople.count;
     
 
 }
